ee8b718fb535fd1827469329307bcadafda1a8ea
EbrahimIslamShuvo <ebrahimislam2003@gmail.com>
2024-03-14 12:35:56 +0600

    ok

--- bin/assignment_1.dart
+++ bin/assignment_1.dart
@@ -1,8 +1,12 @@
 import 'assignment_1class.dart';
 
 main(){
+  //make a object for Media class
   Media ob1=Media();
+  //make a object for song class which have a constructor with a variable
   Song ob2=Song("Rafa");
+  //call media class play method
   ob1.play();
+  //call song class play overwrite method
   ob2.play();
 }
--- bin/assignment_1class.dart
+++ bin/assignment_1class.dart
@@ -1,14 +1,17 @@
+//creating a method class
 class Media{
   play(){
     print("Playing media...");
   }
 }
 
+//creating a song class which extend media class
 class Song extends Media{
   String artist;
 
   Song(this.artist);
 
+  //overwrite the play method
   play(){
     print("Playing song by $artist");
   }
